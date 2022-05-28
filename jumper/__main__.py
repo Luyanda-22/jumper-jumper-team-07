aa2e33b10fc8a8a3a0d865f6366347042497fd03
KobeLemon <104539403+KobeLemon@users.noreply.github.com>
2022-05-27 23:15:20 -0600

    Made modifcations to all files

--- jumper/__main__.py
+++ jumper/__main__.py
@@ -1,4 +1,11 @@
-from game.director import Director
+# Global Imports:
+from game.director import _Director
 
-director = Director()
-director.start_game()
+def __main__():
+    """Calls the director class to play the game"""
+    director = _Director()
+    director._start_game()
+
+# calls the main function to play the game
+if __name__ == '__main__':
+    __main__()
--- jumper/game/Words.py
+++ jumper/game/Words.py
@@ -1,12 +1,17 @@
+# Global Imports:
 import random
 
-'''This is a list of words that the program will choose from. I went to a random word 
+'''This is a list of words that the program will choose from. We went to a random word 
 generator and generated 15 random words to get these.'''
 
-
-#for some reason, the code would only run if I took it out of the class and had it be its own stray function.
-words_list = ["apricot", "grounds", "analyst", "magnetic", "desire", "foster", 
-"aquisition", "provoke", "reserve", "confront", "helpless", "decline", "bargain", 
-"injection", "monopoly"]
-
-words = random.choice(words_list).upper()
+class _RandomWord:
+    """This is a class that creates a list of words that the program will choose from"""
+    def __init__(self):
+        self.words_list = []
+    
+    def _get_random_word(self):
+        self.words_list = ["apricot","grounds","analyst","magnetic","desire","foster",
+                            "aquisition","provoke","reserve","confront","helpless",
+                            "decline","bargain","injection","monopoly"]
+        word = random.choice(self.words_list).upper()
+        return word
